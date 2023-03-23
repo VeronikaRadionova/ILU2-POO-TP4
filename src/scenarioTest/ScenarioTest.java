8229b600115acc39fbc2b2ffb74ef4ea87d750ed
VeronikaRadionova <114427643+VeronikaRadionova@users.noreply.github.com>
2023-03-23 19:54:38 +0100

    2

--- src/scenarioTest/ScenarioTest.java
+++ src/scenarioTest/ScenarioTest.java
@@ -4,15 +4,15 @@ import produit.Produit;
 import produit.Poisson;
 import produit.Sanglier;
 import villagegaulois.Etal;
+import villagegaulois.IEtal;
 
 public class ScenarioTest {
 	public static void main(String[] args) {
-		Etal[] marche = new Etal[3];
-		Etal<Sanglier> etalSanglier = new Etal<>();
-		Etal<Poisson> etalPoisson = new Etal<>(); 
+		IEtal<Produit>[] marche = new IEtal[3];
+		IEtal<Sanglier> etalSanglier = new Etal<>();
+		IEtal<Poisson> etalPoisson = new Etal<>(); 
 		marche[0] = etalSanglier;
 		marche[1] = etalPoisson;
-
 	}
 	
 }
